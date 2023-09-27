875be5e1713fe7e4ae28b14a60a013ffe3c08cd9
adrianparedes477 <adrianjparedes477@gmail.com>
2023-09-27 08:23:53 -0300

    Creando login

---
+++ ProyectoFinalFront/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinalFront.Datos.Dto;
+
+namespace ProyectoFinalFront.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Ingresar(LoginDto login)
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
